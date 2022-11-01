716f484145272716ef6668d86a9ca649eb08b310
Gabriel Guild <77171280+GabrielGuild@users.noreply.github.com>
2022-11-01 15:28:38 -0700

    Update apiCalls.js

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -1,4 +1,4 @@
-const apiUrl = `https://sabrina.onrender.com/api`
+const apiUrl = `https://sabrinaguild.onrender.com/api`
 // const apiUrl = `https://localhost:4000/api`
 
 export const apiCall = async (url, method = 'GET', token, body) => {
