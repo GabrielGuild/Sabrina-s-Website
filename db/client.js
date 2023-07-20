8780b2d82a272b46ec33e48f87520fbb8ad66af7
Gabriel Guild <77171280+GabrielGuild@users.noreply.github.com>
2023-07-20 13:25:49 -0700

    Update client.js

--- db/client.js
+++ db/client.js
@@ -1,13 +1,9 @@
 // Connect to DB
 const {  Pool } = require('pg')
-// const connectionString = 'postgresql://GabrielGuild:v2_3y4C9_7EyVgMhcUN74Xua3s8Zv59C@db.bit.io:5432/GabrielGuild.sabrina?sslmode=require'
+ const connectionString = 'postgres://ybehutku:xZmGQutOjhSfATX_LYAqn6bDtV5RlNjN@otto.db.elephantsql.com/ybehutku'
 
 const pool = new Pool({
-    user: 'GabrielGuild',
-    host: 'db.bit.io',
-    database: 'GabrielGuild/sabrina',
-    password: 'vv2_3y4MR_4aGBreuEdYB5dyVUEzkvcdK',
-    port: 5432
+  connectionString: connectionString,
   });
 
 module.exports = pool;
