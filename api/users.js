0f812a4207a54bbe730d75c1315fcb0249fcaa4e
Gabriel Guild <77171280+GabrielGuild@users.noreply.github.com>
2023-07-21 12:54:33 -0700

    Update users.js

--- api/users.js
+++ api/users.js
@@ -12,9 +12,6 @@ const {
   emailInUseCheck
 } = require('../db');
 
-// ... All your imports and setup ...
-
-//api calls below
 usersRouter.get('/', async (req, res, next) => {
   try {
     const allUsers = await getAllUsers();
@@ -25,81 +22,87 @@ usersRouter.get('/', async (req, res, next) => {
 });
 
 usersRouter.post('/login', async (req, res, next) => {
-  console.log('the request reached the api')
-  const { username, password } = req.body;  
+  const { username, password } = req.body;
+
   if (!username || !password) {
-    res.send({
+    return res.status(400).json({
       error: 'Error',
       name: 'MissingCredentialsError',
-      message: "Missing username or password."
-    })
+      message: "Missing username or password.",
+    });
   }
 
   try {
-    const user = await getUser({username, password});
-console.log('sending the request to the db')
-    if (user.id) {
-      const token = jwt.sign({
-        id: user.id,
-        username: user.username
-      }, JWT_SECRET);
-      res.send({ user, token, message: "you're logged in!" })
-    } else if (user === 'passwordNotValid') {
-      res.send({
+    const user = await getUser({ username, password });
+
+    if (!user || !user.id) {
+      return res.status(404).json({
+        error: 'Error',
+        name: 'UserNotFoundError',
+        message: 'User not found.',
+      });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return res.status(401).json({
         error: 'Error',
         name: 'PasswordNotValid',
-        message: `Incorrect password for the username.`
-      })
-    } else if (user === 'userDoesNotExist') {
-      res.send({
-        error: 'Error',
-        name:'UserNotFoundError',
-        message: 'User not found.'
-      })
-    } else {
-      next({
-        name: 'LoginError',
-        message: 'There was an error during login.'
-      })
+        message: `Incorrect password for the username.`,
+      });
     }
-  } catch ({name, message}) {
-    next({name, message});
+
+    const token = jwt.sign({
+      id: user.id,
+      username: user.username,
+    }, JWT_SECRET);
+
+    res.json({
+      user: { id: user.id, username: user.username }, // Only send necessary user data
+      token,
+      message: "You're logged in!",
+    });
+  } catch (error) {
+    next(error);
   }
 });
 
-
 usersRouter.post('/register', async (req, res, next) => {
-  console.log("The api is running the request")
   try {
     const { username, password, address, fullname, email } = req.body;
 
     if (password.length < 8) {
-      res.status(400).json({
+      return res.status(400).json({
         error: 'PasswordError',
         message: 'Password needs to be at least 8 characters',
       });
-      return;
     }
 
     const _user = await getUserByUsername(username);
     if (_user) {
-      res.status(409).json({
+      return res.status(409).json({
         name: 'UserExistsError',
         message: `Username ${username} is already taken.`,
       });
-      return;
     }
 
     const emailAlreadyInUse = await emailInUseCheck(email);
     if (emailAlreadyInUse) {
-      res.status(409).json({
+      return res.status(409).json({
         name: 'EmailAlreadyUsed',
         message: `The email address ${email} has already been used by another user.`,
       });
-      return;
     }
 
-    const user = await createUser({ username, password, address, fullname, email });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await createUser({
+      username,
+      password: hashedPassword,
+      address,
+      fullname,
+      email,
+    });
 
     const token = jwt.sign(
       {
@@ -112,14 +115,14 @@ usersRouter.post('/register', async (req, res, next) => {
     res.json({
       message: "You are signed up.",
       token,
-      user,
+      user: { id: user.id, username: user.username }, // Only send necessary user data
     });
   } catch (error) {
     next(error);
   }
 });
 
-usersRouter.get('/me', requireLogin, async (req, res, next) => {
+usersRouter.get('/me', requireLogin, (req, res, next) => {
   if (req.user) {
     res.json(req.user);
   } else {
