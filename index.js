ef0ac517b89f22352bf1dbbeca44a58ce868e317
Gabriel Guild <77171280+GabrielGuild@users.noreply.github.com>
2023-07-21 11:28:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+// This is the Web Server
 require("dotenv").config();
 const express = require('express');
 const server = express();
 const apiRouter = require('./api');
-const pg = require('pg');
 
 // enable cross-origin resource sharing to proxy api requests
 // from localhost:3000 to localhost:4000 in local dev env
@@ -14,12 +14,13 @@ const morgan = require('morgan');
 server.use(morgan('dev'));
 
 // handle application/json requests
-server.use(express.urlencoded({extended: false})); 
+server.use(express.urlencoded({extended: false}));
 server.use(express.json());
 
 // here's our static files
 const path = require('path');
 server.use(express.static(path.join(__dirname, 'build')));
+
 
 // here's our API
 server.use('/api', apiRouter);
@@ -29,34 +30,28 @@ server.use((req, res, next) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// bring in the DB connection  
-const client = require('./db/client');
-
-// Connect to the server
-const PORT = process.env.PORT || 4000;
-let handle; // Declare the handle variable outside the callback
-
 // bring in the DB connection
 const { client } = require('./db');
 
-  // Start the server only after successfully connecting to the database
-  handle = server.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}!`);
-  });
-
-  // Close the database connection gracefully when the server is closed
-  process.on('SIGINT', async () => {
-    console.log('Closing client');
-    await client.end();
-    console.log('Database connection closed');
-    process.exit();
-  });
-});
+// connect to the server
+const PORT = process.env.PORT || 4000;
 
 // Error handler next
 server.use((error, req, res, next) => {
   res.status(500);
   res.send(error);
+});
+
+// define a server handle to close open tcp connection after unit tests have run
+const handle = server.listen(PORT, async () => {
+  console.log(`Server is running on ${PORT}!`);
+
+  try {
+    await client.connect();
+    console.log('Database is open for business!');
+  } catch (error) {
+    console.error('Database is closed for repairs!\n', error);
+  }
 });
 
 // export server and handle for routes/*.test.js
