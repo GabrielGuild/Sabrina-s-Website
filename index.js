85976eca57db9cc35cc3acec005225f6b2ca4886
Gabriel Guild <77171280+GabrielGuild@users.noreply.github.com>
2023-07-21 10:52:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// This is the Web Server
 require("dotenv").config();
 const express = require('express');
 const server = express();
@@ -14,13 +13,12 @@ const morgan = require('morgan');
 server.use(morgan('dev'));
 
 // handle application/json requests
-server.use(express.urlencoded({extended: false}));
+server.use(express.urlencoded({extended: false})); 
 server.use(express.json());
 
 // here's our static files
 const path = require('path');
 server.use(express.static(path.join(__dirname, 'build')));
-
 
 // here's our API
 server.use('/api', apiRouter);
@@ -30,11 +28,31 @@ server.use((req, res, next) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// bring in the DB connection
-const { pool } = require('./db');
+// bring in the DB connection  
+const client = require('./db/client');
 
-// connect to the server
+// Connect to the server
 const PORT = process.env.PORT || 4000;
+
+client.connect((err) => {
+  if (err) {
+    console.error('Could not connect to Postgres:', err);
+    process.exit(1);
+  }
+
+  // Start the server only after successfully connecting to the database
+  const handle = server.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}!`);
+  });
+
+  // Close the database connection gracefully when the server is closed
+  process.on('SIGINT', async () => {
+    console.log('Closing client');
+    await client.end();
+    console.log('Database connection closed');
+    process.exit();
+  });
+});
 
 // Error handler next
 server.use((error, req, res, next) => {
@@ -42,19 +60,5 @@ server.use((error, req, res, next) => {
   res.send(error);
 });
 
-// define a server handle to close open tcp connection after unit tests have run
-const handle = server.listen(PORT, async () => {
-  console.log(`Server is running on ${PORT}!`);
-  pool.on('connect', () => {
-    console.log('Database is open for business!');
-  });
-});
-
-process.on('SIGINT', async () => {
-    console.log('Closing pool');
-    await pool.end();
-    process.exit()
-  });
-
 // export server and handle for routes/*.test.js
- module.exports = { server, handle };
+module.exports = { server, handle };
