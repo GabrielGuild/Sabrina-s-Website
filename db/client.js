01839a233a5dc8f43f0b085d0c4270fa248cfb9a
Gabriel Guild <77171280+GabrielGuild@users.noreply.github.com>
2023-07-21 10:52:51 -0700

    Update client.js

--- db/client.js
+++ db/client.js
@@ -1,9 +1,22 @@
 // Connect to DB
-const {  Pool } = require('pg')
- const connectionString = 'postgres://ybehutku:xZmGQutOjhSfATX_LYAqn6bDtV5RlNjN@otto.db.elephantsql.com/ybehutku'
+var pg = require('pg');
 
-const pool = new Pool({
-  connectionString: connectionString,
+var conString = 'postgres://ybehutku:xZmGQutOjhSfATX_LYAqn6bDtV5RlNjN@otto.db.elephantsql.com/ybehutku';
+var client = new pg.Client(conString);
+
+client.connect(function(err) {
+  if (err) {
+    return console.error('could not connect to postgres', err);
+  }
+
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if (err) {
+      return console.error('error running query', err);
+    }
+
+    console.log(result.rows[0].theTime); // >> output: 2018-08-23T14:02:57.117Z
+    client.end();
   });
+});
 
-module.exports = pool;
+module.exports = client;
